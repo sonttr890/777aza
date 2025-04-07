61545158477813bd816cd0438ee04d1079b97a26
sonttr890 <147316444+sonttr890@users.noreply.github.com>
2025-04-07 12:42:18 +0700

    Create input.tsx

---
+++ components/ui/input.tsx
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input({ className, ...props }: InputProps) {
+  return (
+    <input
+      className={cn(
+        "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500",
+        className
+      )}
+      {...props}
+    />
+  );
+}
